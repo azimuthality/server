9103ac73bedd6356be05b42a2a89a9af823efe13
Asim033 <asimazizov162003@gmail.com>
2022-03-19 19:43:20 +0300

    2commit

---
+++ Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace HTTPServer
+{
+    public class Server
+    {
+ 
+            TcpListener Listener;
+            public Server(int Port)
+            {
+                Listener = new TcpListener(IPAddress.Any, Port);
+                Listener.Start();
+                while (true)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+
+                }
+            }
+            static void ClientThread(Object StateInfo)
+            {
+                new Client((TcpClient)StateInfo);
+            }
+            ~Server()
+            {
+                if (Listener != null)
+                {
+                    Listener.Stop();
+                }               
+            }
+        }
+    
+}
